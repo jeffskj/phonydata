33d1e2bb93d46a9da84a6a430fd062f465901c5f
Jeff Skjonsby <jskjons@rei.com>
2014-10-24 18:54:05 -0700

    can insert super basic dataset into database now

--- src/main/groovy/io/github/phonydata/DatabaseDataSetWriter.groovy
+++ src/main/groovy/io/github/phonydata/DatabaseDataSetWriter.groovy
@@ -2,26 +2,58 @@ package io.github.phonydata
 
 import groovy.sql.Sql
 
-import java.sql.Connection
 import java.sql.DatabaseMetaData
+
+import javax.sql.DataSource
 
 class DatabaseDataSetWriter implements DataSetWriter {
     private Sql sql
+    private String quote
+    private Set<String> keywords
     
-    DatabaseDataSetWriter(Connection conn) {
-        sql = new Sql(conn)
+    DatabaseDataSetWriter(DataSource dataSource) {
+        sql = new Sql(dataSource)
+        quote = dataSource.connection.metaData.identifierQuoteString
+        keywords = getKeywords(dataSource.connection.metaData)
     }
     
     public void write(DataSet ds) {
         def fks = findForeignKeys(ds.tables.keySet())
-        def sortedTables = topologicalSort(fks) 
+        def sortedTables = topologicalSort(fks)
+         
+        sql.withBatch {
+            sortedTables.each { tableName ->
+                sql.cacheStatements { 
+                    def table = ds.tables[tableName]
+                    def insertStatement = createInsertStatement(table)
+                    table.each {
+                        sql.executeInsert(insertStatement, it.data);
+                    }
+                }
+            }    
+        }
+        
     }
 
+    String createInsertStatement(Table t) {
+        def cols = t.columns.toList()
+        def quotedCols = cols.collect { q(it) }
+        def paramCols = cols.collect { ':' + it }
+        return "insert into ${t.name} (${quotedCols.join(',')}) values (${paramCols.join(',')});".toString()
+    }
+    
+    private String q(String s) {
+        if (keywords.contains(s)) {
+            return "${quote}${s}${quote}".toString()
+        }
+        return s
+    }
+    
     static def topologicalSort(Map fks) {
         def sorted = []
         def counter = fks.size()
         while (!fks.isEmpty()) {
-            if (counter-- < 0) {
+            if (counter-- < 0) { //TODO: consider warning then just throwing the rest in the sorted list
                 throw new IllegalStateException("unable to topologically sort cyclical table references!")
             }
             def unReferenced = fks.findAll { t, refs -> refs.isEmpty() }.keySet()
@@ -32,14 +64,23 @@ class DatabaseDataSetWriter implements DataSetWriter {
         return sorted
     }
     
+    static def getKeywords(DatabaseMetaData md) {
+        def keywords = [] as Set
+        keywords.addAll(md.SQLKeywords.split(','))
+        println keywords
+        keywords.addAll(SQL_2K3_KEYWORDS)
+        return keywords
+        
+    }
+    
     private Map findForeignKeys(tables) {
-        def metaData = sql.connection.metaData
+        def metaData = sql.dataSource.connection.metaData
         return tables.collectEntries {
             [it, getFksForTable(metaData, it)]
         }
     }
     
-    private List getFksForTable(DatabaseMetaData metaData, tableName) {
+    private Set getFksForTable(DatabaseMetaData metaData, tableName) {
         def rs = metaData.getImportedKeys(null, null, tableName)
         def fks = [] as Set
         
@@ -48,4 +89,26 @@ class DatabaseDataSetWriter implements DataSetWriter {
         }
         return fks
     }
+    
+    private static final Set<String> SQL_2K3_KEYWORDS = [
+        'ADD','ALL','ALLOCATE','ALTER','AND','ANY','ARE','ARRAY','AS','ASENSITIVE','ASYMMETRIC','AT','ATOMIC',
+        'AUTHORIZATION','BEGIN','BETWEEN','BIGINT','BINARY','BLOB','BOOLEAN','BOTH','BY','CALL','CALLED','CASCADED',
+        'CASE','CAST','CHAR','CHARACTER','CHECK','CLOB','CLOSE','COLLATE','COLUMN','COMMIT','CONNECT','CONSTRAINT',
+        'CONTINUE','CORRESPONDING','CREATE','CROSS','CUBE','CURRENT','CURRENT_DATE','CURRENT_DEFAULT_TRANSFORM_GROUP',
+        'CURRENT_PATH','CURRENT_ROLE','CURRENT_TIME','CURRENT_TIMESTAMP','CURRENT_TRANSFORM_GROUP_FOR_TYPE','CURRENT_USER',
+        'CURSOR','CYCLE','DATE','DAY','DEALLOCATE','DEC','DECIMAL','DECLARE','DEFAULT','DELETE','DEREF','DESCRIBE',
+        'DETERMINISTIC','DISCONNECT','DISTINCT','DOUBLE','DROP','DYNAMIC','EACH','ELEMENT','ELSE','END','END','EXEC',
+        'ESCAPE','EXCEPT','EXEC','EXECUTE','EXISTS','EXTERNAL','FALSE','FETCH','FILTER','FLOAT','FOR','FOREIGN','FREE','FROM',
+        'INNER','INOUT','INPUT','INSENSITIVE','INSERT','INT','INTEGER','INTERSECT','INTERVAL','INTO','IS','ISOLATION','JOIN',
+        'LANGUAGE','LARGE','LATERAL','LEADING','LEFT','LIKE','LOCAL','LOCALTIME','LOCALTIMESTAMP','MATCH','MEMBER','MERGE',
+        'METHOD','MINUTE','MODIFIES','MODULE','MONTH','MULTISET','NATIONAL','NATURAL','NCHAR','NCLOB','NEW','NO','NONE','NOT',
+        'NULL','NUMERIC','OF','OLD','ON','ONLY','OPEN','OR','ORDER','OUT','OUTER','OUTPUT','OVER','OVERLAPS','PARAMETER',
+        'PARTITION','PRECISION','PREPARE','PRIMARY','PROCEDURE','RANGE','READS','REAL','RECURSIVE','REF','REFERENCES',
+        'REFERENCING','REGR_AVGX','REGR_AVGY','REGR_COUNT','REGR_INTERCEPT','REGR_R2','REGR_SLOPE','REGR_SXX','REGR_SXY',
+        'REGR_SYY','RELEASE','RESULT','RETURN','RETURNS','REVOKE','RIGHT','ROLLBACK','ROLLUP','ROW','ROWS','SAVEPOINT','SCROLL',
+        'SEARCH','SECOND','SELECT','SENSITIVE','SESSION_USER','SET','SIMILAR','SMALLINT','SOME','SPECIFIC','SPECIFICTYPE','SQL',
+        'SQLEXCEPTION','SQLSTATE','SQLWARNING','START','STATIC','SUBMULTISET','SYMMETRIC','SYSTEM','SYSTEM_USER','TABLE','THEN',
+        'TIME','TIMESTAMP','TIMEZONE_HOUR','TIMEZONE_MINUTE','TO','TRAILING','TRANSLATION','TREAT','TRIGGER','TRUE','UESCAPE','UNION',
+        'UNIQUE','UNKNOWN','UNNEST','UPDATE','UPPER','USER','USING','VALUE','VALUES','VAR_POP','VAR_SAMP','VARCHAR','VARYING','WHEN',
+        'WHENEVER','WHERE','WIDTH_BUCKET','WINDOW','WITH','WITHIN','WITHOUT','YEAR'] as Set
 }
--- src/test/groovy/io/github/phonydata/DatabaseDataSetWriterTest.groovy
+++ src/test/groovy/io/github/phonydata/DatabaseDataSetWriterTest.groovy
@@ -1,13 +1,17 @@
 package io.github.phonydata;
 
 import static org.junit.Assert.*
+import groovy.sql.Sql
 
+import javax.sql.DataSource
+
+import org.h2.jdbcx.JdbcConnectionPool
 import org.junit.Test
 
 class DatabaseDataSetWriterTest {
 
     @Test
-    public void canTopologicalSort() {
+    void canTopologicalSort() {
         def sorted = DatabaseDataSetWriter.topologicalSort(a: [], b: [], c:[])
         assertEquals(['a', 'b', 'c'], sorted)
         
@@ -24,5 +28,22 @@ class DatabaseDataSetWriterTest {
     @Test(expected=RuntimeException)
     void disallowsSortingCyclicalGraph() {
         DatabaseDataSetWriter.topologicalSort(a: ['b'], b: ['c'], c:['a'])
+    }
+    
+    @Test
+    void canInsertSimpleDataSet() {
+        def ds = new GroovyDataSet("test(id:1, name: 'joe blow')")
+        def writer = new DatabaseDataSetWriter(createConnection())
+        writer.write(ds)
+        assertEquals(1, writer.sql.rows("select * from test").size())
+    }
+    
+    private DataSource createConnection() {
+        Class.forName("org.h2.Driver");
+        JdbcConnectionPool cp = JdbcConnectionPool.create("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "sa");
+        def sql = new Sql(cp.dataSource)
+        sql.cacheConnection = true
+        sql.executeUpdate("create table test (id int primary key, name varchar(255))")
+        return cp.dataSource
     }
 }
